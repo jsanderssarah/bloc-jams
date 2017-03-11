7fa205a9a5eca1f41f4e94e6a49c52bf109f0384
Sarah Bentzen <Sarah@Sarahs-MacBook-Pro.local>
2017-03-11 12:19:23 -0600

    updated forEach function

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,30 +1,28 @@
 var pointsArray = document.getElementsByClassName('point');
- 
-var animatePoints = function(points) {
 
-    var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-        forEach(pointsArray, revealPoints);
-        }
-    };
-
+var revealPoint = function() {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+var animatePoints = function() {
+    forEach(pointsArray, revealPoint);
+};
+    
 window.onload = function() {
     // Automatically animate points on a tall screen scrolling can't trigger the animation
      if (window.innerHeight > 950) {
          animatePoints(pointsArray);
      }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+    
      window.addEventListener('scroll', function(event) {
          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
              animatePoints(pointsArray);   
          }
      });
  } 
+    
      
 
      
--- scripts/utilites.js
+++ scripts/utilites.js
@@ -1,18 +1,5 @@
-var pointsArray = document.getElementsByClassName('point');
-
-function forEach(pointsArray, callback){
-   for(var i = 0; i < pointsArray; i++){
+function forEach(array, callback){
+   for(var i = 0; i < array.length; i++){
        callback(pointArray[i]);
    } 
 }
-
-var revealPoint = function(index) {
-        points[index].style.opacity = 1;
-        points[index].style.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-    }
-        for(var i = 0; i < points.length; i++){
-            revealPoint(i);
-
-forEach(pointsArray, revealPoints);
