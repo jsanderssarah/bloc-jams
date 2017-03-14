19e3a17690c82907b89e91ae70ba51ff499cb563
Sarah Bentzen <Sarah@Sarahs-MacBook-Pro.local>
2017-03-14 09:55:41 -0500

    forEach function fixed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,10 @@
 var pointsArray = document.getElementsByClassName('point');
 
-var revealPoint = function() {
-    pointsArray.style.opacity = 1;
-    pointArray.style.transform = "scaleX(1) translateY(0)";
-    pointsArrayray.style.msTransform = "scaleX(1) translateY(0)";
-    pointsArray.style.WebkitTransform = "scaleX(1) translateY(0)";
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
 var animatePoints = function() {
     forEach(pointsArray, revealPoint);
@@ -15,17 +15,14 @@ window.onload = function() {
      if (window.innerHeight > 950) {
          animatePoints(pointsArray);
      }
-    
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
      window.addEventListener('scroll', function(event) {
          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
              animatePoints(pointsArray);   
          }
      });
  } 
-    
-     
-
-     
     
     
     
--- scripts/utilites.js
+++ scripts/utilites.js
@@ -1,5 +1,5 @@
 function forEach(array, callback){
    for(var i = 0; i < array.length; i++){
-       callback(pointArray[i]);
+       callback(array[i]);
    } 
 }
