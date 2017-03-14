e27d60d1acf20b614bc2a59f6aab76d4466d382f
Sarah Bentzen <Sarah@Sarahs-MacBook-Pro.local>
2017-03-14 09:41:35 -0500

    fixed script link

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,10 +1,10 @@
 var pointsArray = document.getElementsByClassName('point');
 
 var revealPoint = function() {
-    point.style.opacity = 1;
-    point.style.transform = "scaleX(1) translateY(0)";
-    point.style.msTransform = "scaleX(1) translateY(0)";
-    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+    pointsArray.style.opacity = 1;
+    pointArray.style.transform = "scaleX(1) translateY(0)";
+    pointsArrayray.style.msTransform = "scaleX(1) translateY(0)";
+    pointsArray.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
 var animatePoints = function() {
     forEach(pointsArray, revealPoint);
