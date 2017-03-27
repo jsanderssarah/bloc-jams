95518bb03ec5b17b0b18354846d8fa4f69d4df1c
Sarah Bentzen <Sarah@Sarahs-MBP.hsd1.tn.comcast.net>
2017-03-26 22:05:11 -0500

    Code issue resolved

--- scripts/album.js
+++ scripts/album.js
@@ -28,35 +28,35 @@
      ]
  };
 var createSongRow = function(songNumber, songName, songLength) {
-     var $template =
+     var template =
         '<tr class="album-view-song-item">'
       + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
       + '  <td class="song-item-title">' + songName + '</td>'
       + '  <td class="song-item-duration">' + songLength + '</td>'
       + '</tr>'
       ;
-    var $row = $template;
-     var clickHandler = function() {
-        var songNumber = $(this).attr('data-song-number');
+    var $row = $(template);
+    var clickHandler = function() {
+      var songNumber = $(this).attr('data-song-number');
 
 	  if (currentlyPlayingSong !== null) {
 		// Revert to song number for currently playing song because user started playing new song.
 		var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
 		currentlyPlayingCell.html(currentlyPlayingSong);
-	 }
-	 if (currentlyPlayingSong !== songNumber) {
+	  }
+	  if (currentlyPlayingSong !== songNumber) {
 		// Switch from Play -> Pause button to indicate new song is playing.
 		$(this).html(pauseButtonTemplate);
 		currentlyPlayingSong = songNumber;
-	 } else if (currentlyPlayingSong === songNumber) {
+	  } else if (currentlyPlayingSong === songNumber) {
 		// Switch from Pause -> Play button to pause currently playing song.
 		$(this).html(playButtonTemplate);
 		currentlyPlayingSong = null;
-	 }
-};
-     };
+	  }
+    };
+
      
-   var onHover = function(event) {
+    var onHover = function(event) {
         var songNumberCell = $(this).find('.song-item-number');
         var songNumber = songNumberCell.attr('data-song-number');
 
@@ -79,7 +79,7 @@ var createSongRow = function(songNumber, songName, songLength) {
      $row.hover(onHover, offHover);
      // #3
      return $row;
- };
+};
 var setCurrentAlbum = function(album) {
      var $albumTitle = $('.album-view-title');
      var $albumArtist = $('.album-view-artist');
